757fcfb6e14bd892f472ed56319404ee61d26af2
Abdelrahim Adam <133844991+AbdelrahimAdam@users.noreply.github.com>
2025-12-07 03:38:46 +0200

    Create Dashboard.vue

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- Your dashboard content goes here -->
+    <h1>Dashboard Content</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
