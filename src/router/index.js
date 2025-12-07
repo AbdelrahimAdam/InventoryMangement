19ca7192e515b54d9ed8992c7e2e4a60aa579e12
Abdelrahim Adam <133844991+AbdelrahimAdam@users.noreply.github.com>
2025-12-07 03:51:59 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -59,7 +59,7 @@ const routes = [
   {
     path: '/inventory/add',
     name: 'AddInventory',
-    component: () => import('@/views/AddItem.vue'),
+    component: () => import('@/views/inventory/AddItemModal.vue'),
     meta: { 
       requiresAuth: true,
       allowedRoles: ['superadmin', 'warehouse_manager'],
@@ -72,7 +72,7 @@ const routes = [
   {
     path: '/inventory/edit/:id',
     name: 'EditInventory',
-    component: () => import('@/views/EditItem.vue'),
+    component: () => import('@/views/inventory/EditItemModal.vue'),
     meta: { 
       requiresAuth: true,
       allowedRoles: ['superadmin', 'warehouse_manager'],
