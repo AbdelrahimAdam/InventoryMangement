6a308cd48c0805d8e76c487ea65ecdc3105d65f5
Abdelrahim Adam <133844991+AbdelrahimAdam@users.noreply.github.com>
2025-12-08 11:17:53 +0200

    Add ProfileContent component with template and script

---
+++ src/components/ProfileContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="space-y-6">
+    <h1 class="text-2xl font-bold text-gray-900 dark:text-white">الملف الشخصي</h1>
+    <p class="text-gray-600 dark:text-gray-400">إدارة حسابك وإعدادات النظام</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileContent'
+}
+</script>
