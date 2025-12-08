bc8091d87d5dd2862b16912cefef2202751aac94
Abdelrahim Adam <133844991+AbdelrahimAdam@users.noreply.github.com>
2025-12-08 11:17:09 +0200

    Add TransactionsContent component for transaction tracking

---
+++ src/components/TransactionsContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="space-y-6">
+    <h1 class="text-2xl font-bold text-gray-900 dark:text-white">سجل الحركات</h1>
+    <p class="text-gray-600 dark:text-gray-400">عرض وتتبع جميع حركات المخزون</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionsContent'
+}
+</script>
